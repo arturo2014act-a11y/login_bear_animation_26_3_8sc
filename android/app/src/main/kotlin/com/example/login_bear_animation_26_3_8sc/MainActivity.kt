a8d7c581c380eb67236b6ac43d950fbc9c835519
Alberto Tenreiro <arturo2014.act@gmail.com>
2026-09-08 20:47:21 -0600

    fear(rive)animation added

---
+++ android/app/src/main/kotlin/com/example/login_bear_animation_26_3_8sc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.login_bear_animation_26_3_8sc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:login_bear_animation_26_3_8sc/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Opcional: quita la etiqueta de "Debug"
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
